100995d7ec71ff9e81d1dee01823536224a0d7fb
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-05 00:54:01 +0800

    first day of review

---
+++ review.js
@@ -0,0 +1,21 @@
+let name = "Alephzain";
+console.log(name);
+let age = 20;
+console.log(age);
+let isStudent = false;
+console.log(isStudent);
+if (isStudent) {
+  console.log("Welcome to the school");
+} else {
+  console.log("Access denied");
+}
+let isHungry = true;
+if (isHungry) {
+  console.log("Let's eat!");
+}
+else {  console.log("Im full");
+}
+
+
+let hobbies = ["coding", "gaming", "traveling"];
+console.log(hobbies);
