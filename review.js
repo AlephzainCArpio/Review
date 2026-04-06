3697c9a1814a2cdd11bafa3ac63e8c1749c80ef4
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-07 00:32:07 +0800

    learning basic function syntax

--- review.js
+++ review.js
@@ -1,6 +1,6 @@
 let name = "Alephzain";
 console.log(name);
-let age = 20;
+let age = 24;
 console.log(age);
 let isStudent = false;
 console.log(isStudent);
@@ -16,7 +16,64 @@ if (isHungry) {
 } else {
   console.log("I'm full");
 }
+let score = 85;
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C");
+}
 
 
 let hobbies = ["coding", "gaming", "traveling"];
 console.log(hobbies);
+
+
+
+function sayHello(name) {
+  console.log("Hello, " + name + "!");
+}
+let message = sayHello("Alephzain");
+
+function checkHunger(isHungry) {
+  if (isHungry) {
+    console.log("Let's eat!");
+  } else {
+    console.log("I'm full");
+  }
+}
+checkHunger(true);
+checkHunger(false);
+
+function introduce(name, age) {
+  console.log("My name is " + name + " and I am " + age + " years old.");
+}
+introduce("Alephzain", 24);
+introduce("Alice", 30);
+
+function add(a, b) {
+  let result = a + b;
+  console.log(result);
+}
+
+add(2, 3);
+add(10, 5);
+function multiply(a, b) {
+  let result = a * b;
+  console.log(result);
+}
+multiply(2, 3);
+multiply(2, 100);
+
+function checkAge(age) {
+  if (age >= 18) {
+    console.log("You are an adult.");
+  } else {
+    console.log("You are a minor.");
+  }
+}
+checkAge(24);
+checkAge(16);
+checkAge(18);
+
