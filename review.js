576cf0c518812eac36a84cc538e64101791e5491
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-18 02:27:16 +0800

    applied function to all student objects to avoid duplication

--- review.js
+++ review.js
@@ -346,7 +346,7 @@ studentscores.forEach(student => {
 
 let car = {
   brand:"Toyota",
-  speed:"120",
+  speed:120,
   getInfo: function() {
     return this.brand + " - " + this.speed + " km/h";
   }
@@ -364,3 +364,21 @@ let studentInfo = {
   }
 }
 console.log(studentInfo.name + " = " + studentInfo.getGrade());
+
+function getGrade() {
+  if (this.score >= 90) return "Excellent";
+  else if (this.score >= 75) return "Good";
+  else if (this.score >= 50) return "Pass";
+  else return "Failed";
+}
+
+let students = [
+  { name: "John", score: 90, getGrade },
+  { name: "Jane", score: 60, getGrade },
+  { name: "Mark", score: 75, getGrade },
+  { name: "Ana", score: 45, getGrade }
+];
+
+students.forEach(student => {
+  console.log(student.name + " = " + student.getGrade());
+});
