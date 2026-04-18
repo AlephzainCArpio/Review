05e1cfead0cb5c3f3536cef0eab5c4541a1eeb14
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-19 05:50:29 +0800

    learning map

--- review.js
+++ review.js
@@ -403,3 +403,7 @@ const printStudent = (student) => {
 
 const student = { name: "John", score: 90 };
 printStudent(student);
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled); // [2, 4, 6, 8]
