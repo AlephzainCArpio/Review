b50f95dcdb123443d45f3920938b9ba1dc218602
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-20 05:11:21 +0800

    task 1 easy mode, Back button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const title = document.getElementById("title");
 const btn = document.getElementById("btn");
-
 btn.addEventListener("click", () => {
   title.textContent = "You clicked the button!";
 });
+const backBtn = document.getElementById("btn2");
+backBtn.addEventListener("click", () => {
+  title.textContent = "Hello World";
+}); 
