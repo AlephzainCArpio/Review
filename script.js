54c713636f77c1c17d4e6ad4991b91eb2937a0b6
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-20 05:23:14 +0800

    task 2 Medium, Added an input field to HTML, that changes the title

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const backBtn = document.getElementById("btn2");
 backBtn.addEventListener("click", () => {
   title.textContent = "Hello World";
 }); 
+const input = document.getElementById("input");
+const btn3 = document.getElementById("btn3");
+
+btn3.addEventListener("click", () => {
+  title.textContent = input.value;
+});
