b005aadcd46e5313655f9d008d08c8b60dfce749
AlephzainCArpio <carpioalephzain@gmail.com>
2026-04-29 09:02:29 +0800

    Fix: indentation

--- This-Keyword/tasks.js
+++ This-Keyword/tasks.js
@@ -3,8 +3,8 @@ const car = {
   brand: "Toyota",
   model: "Supra",
   year: 2020,
-    getCarInfo: function () {
-        return this.year + " " + this.brand + " " + this.model;
-    }
+  getCarInfo: function () {
+    return this.year + " " + this.brand + " " + this.model;
+  }
 };
 console.log(car.getCarInfo());
